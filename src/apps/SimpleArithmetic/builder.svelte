e2fd2d775202830dc042adb9aa73a92835efa397
MathyouMB <matthewmacraebovell@outlook.com>
2022-08-18 23:18:50 -0400

    Add custom exercise builder

---
+++ src/apps/SimpleArithmetic/builder.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Checkbox from "../../components/Checkbox.svelte";
+  import Button from "../../components/Button.svelte";
+  export let exercises;
+
+  let selected = [];
+</script>
+
+<div>
+  {#each exercises as exercise}
+    <div>
+      <Checkbox bind:group={selected} value={exercise.id} />
+      {exercise.title}
+    </div>
+  {/each}
+</div>
+
+<br />
+
+{#if selected.length > 0}
+  <a href={`./exercises/custom?exercises=${selected.join(",")}`}
+    ><Button label="Start" /></a
+  >
+{/if}
---
+++ src/components/Checkbox.svelte
@@ -0,0 +1,31 @@
+<script>
+  // Source: https://svelte.dev/repl/de117399559f4e7e9e14e2fc9ab243cc?version=3.12.1
+
+  export let group;
+  export let value;
+  export let checked;
+
+  $: updateChekbox(group);
+  $: updateGroup(checked);
+
+  function updateChekbox(group) {
+    checked = group.indexOf(value) >= 0;
+  }
+
+  function updateGroup(checked) {
+    const index = group.indexOf(value);
+    if (checked) {
+      if (index < 0) {
+        group.push(value);
+        group = group;
+      }
+    } else {
+      if (index >= 0) {
+        group.splice(index, 1);
+        group = group;
+      }
+    }
+  }
+</script>
+
+<input type="checkbox" bind:checked {value} />
