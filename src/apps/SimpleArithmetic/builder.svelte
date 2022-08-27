dc79896f83a360cf3d8ad9c1a58202725d1c476f
Matthew MacRae-Bovell <matthew.macraebovell@shopify.com>
2022-08-27 18:22:14 -0400

    Update builder component

--- src/apps/SimpleArithmetic/builder.svelte
+++ src/apps/SimpleArithmetic/builder.svelte
@@ -4,10 +4,34 @@
   export let exercises;
 
   let selected = [];
+  let arithmeticExercises = exercises.filter(
+    (exercise) => exercise.format === "simple-arithmetic"
+  );
+  let squareExercises = exercises.filter(
+    (exercise) => exercise.format === "squares"
+  );
+  let percentageExercises = exercises.filter(
+    (exercise) => exercise.format === "percentages"
+  );
 </script>
 
 <div>
-  {#each exercises as exercise}
+  <h2>Arithmetic</h2>
+  {#each arithmeticExercises as exercise}
+    <div>
+      <Checkbox bind:group={selected} value={exercise.id} />
+      {exercise.title}
+    </div>
+  {/each}
+  <h2>Squares</h2>
+  {#each squareExercises as exercise}
+    <div>
+      <Checkbox bind:group={selected} value={exercise.id} />
+      {exercise.title}
+    </div>
+  {/each}
+  <h2>Percentages</h2>
+  {#each percentageExercises as exercise}
     <div>
       <Checkbox bind:group={selected} value={exercise.id} />
       {exercise.title}
@@ -18,6 +42,7 @@
 <br />
 
 {#if selected.length > 0}
+  <br />
   <a href={`./exercises/custom?exercises=${selected.join(",")}`}
     ><Button label="Start" /></a
   >
